df48915e5e03735e4b9ef32d9cf30f499be61828
Anitta Siby <anittasiby002@gmail.com>
2023-02-25 00:25:28 +0530

    Update pattern_forLoop.py

--- pattern_forLoop.py
+++ pattern_forLoop.py
@@ -1,11 +1,8 @@
 '''
 program to print the pattern:-
 1
-
 1 2
-
 1 2 3
-
 1 2 3 4
 '''
 
@@ -13,4 +10,4 @@ for i in range (1,5):
     #print("\ni = ", i)
     for j in range (1,i+1):
         print(j, end=' ')
-    print('\n')
+    print('')
