1ee417b6b4107a6eafb0587dcf904330b1e7f310
Anitta Siby <anittasiby002@gmail.com>
2023-06-19 19:56:59 +0530

    Add files via upload

---
+++ module 5 - data processing/p1_numpy_Arrays.py
@@ -0,0 +1,15 @@
+import numpy as np 
+arr = np.array([1, 2, 3, 4, 5]) 
+print(arr) 
+
+# We can also pass a tuple in the array function to create an array.
+arr2 = np.array((3, 4, 5)) 
+print(arr2) 
+
+# Two Dimensional Arrays
+arr3 = np.array([[1, 2, 3], [4, 5, 6]]) 
+print(arr3)
+
+# Three Dimensional Arrays
+arr4 = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(arr4) 
---
+++ module 5 - data processing/p2_numpy_arithmetic_operations.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+a = np.array([7,3,4,5,1])
+b = np.array([3,4,5,6,7])
+
+# add or +
+print(np.add(a, b))
+print(a+b)
+
+# subtract or -
+print(np.subtract(a, b))
+print(a-b)
+
+# mutiply
+print(np.multiply(a, b))
+print(a*b)
+
+# divide
+print(np.divide(a,b))
+print(a/b)
+
+# remainder 
+print(np.remainder(a,b))
+
+# mod
+print(np.mod(a,b))
+
+# power
+print(np.power(a,b))
