cb463c31f767d8cd25f81df031ee1ecafd0566e9
Anitta Siby <anittasiby002@gmail.com>
2023-07-18 22:24:32 +0530

    Update and rename p1_numpy_Arrays.py to p01_numpy_Arrays.py

--- module 5 - data processing/p01_numpy_Arrays.py
+++ module 5 - data processing/p01_numpy_Arrays.py
@@ -1,15 +1,15 @@
 import numpy as np 
 arr = np.array([1, 2, 3, 4, 5]) 
-print(arr) 
+print("array1 =\n",arr) 
 
 # We can also pass a tuple in the array function to create an array.
 arr2 = np.array((3, 4, 5)) 
-print(arr2) 
+print("\narray2 =\n",arr2) 
 
 # Two Dimensional Arrays
 arr3 = np.array([[1, 2, 3], [4, 5, 6]]) 
-print(arr3)
+print("\narray3 =\n",arr3)
 
 # Three Dimensional Arrays
 arr4 = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
-print(arr4) 
+print("\narray4 =\n",arr4) 
