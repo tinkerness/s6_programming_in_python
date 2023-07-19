fcd01aae094bf95933f4da6e436b76721ef15229
Anitta Siby <anittasiby002@gmail.com>
2023-07-19 22:32:28 +0530

    Add files via upload

---
+++ module 3 - graphics/turtle graphics/00_methods.py
@@ -0,0 +1,13 @@
+import turtle
+t=turtle.Turtle()
+
+# Set the orientation of the turtle to to_angle.
+# 0 - east , 90 - north , 180 - west , 270 - south
+t.setheading(45)
+# Move the turtle forward by the specified distance.
+t.forward(50)
+# Turn turtle left by angle units.
+t.left(90)
+t.forward(50)
+
+turtle.done()
